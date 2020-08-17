84082886db991cbe5c8451d39b4bcf8eb3519f6c
DiegoOTdC <studioartdiego@gmail.com>
2020-08-17 14:19:56 +0200

    finish setup route get /analyse/barcode/:barcodeId, add utils analyseBarcode for getting data from api

--- routers/analyse.js
+++ routers/analyse.js
@@ -6,20 +6,24 @@ const router = new Router();
 router.post("/image", async (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   try {
-    const imageLabel = await analyseImage(imageUrl);
-    res.send({ imageLabel });
+    const labels = await analyseImage(imageUrl);
+    console.log("do we get he labels?", labels);
+    res.send({ labels });
   } catch (e) {
     res.status(400).send({ message: "Sorry! Something went wrong" });
     next(e);
   }
 });
 
-router.get("/barcode", async (req, res, next) => {
-  const barcode = 8710398159458;
-  try {
-    const barcodeLabels = await analyseBarcode(barcode);
+router.get("/barcode/:barcodeId", async (req, res, next) => {
+  console.log("do we het here?");
+  const barcode = req.params.barcodeId;
+  console.log("barcode in route?", barcode);
 
-    res.send({ barcodeLabels });
+  try {
+    const labels = await analyseBarcode(barcode);
+    console.log("what is in labels here?", labels);
+    res.send(labels);
   } catch (e) {
     res.status(400).send({ message: "Sorry! Something went wrong" });
     next(e);
--- utils/index.js
+++ utils/index.js
@@ -22,9 +22,10 @@ const analyseImage = async (image) => {
       },
     };
     const [result] = await client.labelDetection(request);
-    const allLabels = result.labelAnnotations.map((item) => item.description);
+    console.log("what is result?", result);
+    const labels = result.labelAnnotations.map((item) => item.description);
 
-    return allLabels;
+    return labels;
   } catch (e) {
     console.log(e);
   }
@@ -32,6 +33,30 @@ const analyseImage = async (image) => {
 
 const analyseBarcode = async (barcode) => {
   console.log("what is barcode?", barcode);
+
+  const response = await axios.get(
+    `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`,
+    { headers: { "User-Agent": "Niles - React Native - Version 1.0" } }
+  );
+
+  // console.log("What is the response?", response.data.product);
+
+  const { product_name, categories_tags, brands } = response.data.product;
+  console.log("product name:", product_name);
+  console.log("catergory_tags", categories_tags);
+  console.log("brands", brands);
+
+  const labels = categories_tags.map((tag) => {
+    const tagsplit = tag.split(":");
+
+    return tagsplit[1];
+  });
+
+  console.log("are these tags without 'en:' ?", labels);
+
+  const nameAndLabels = { name: product_name, labels: labels };
+
+  return nameAndLabels;
 
   try {
   } catch (e) {
