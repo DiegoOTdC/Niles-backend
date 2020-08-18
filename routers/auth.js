5d3617acfbbc1fb551c73d63dc82acc40067ed89
DiegoOTdC <studioartdiego@gmail.com>
2020-08-18 22:27:22 +0200

    routes were already there because I used a template, cleanup unnecessary code

--- routers/auth.js
+++ routers/auth.js
@@ -1,7 +1,6 @@
 const bcrypt = require("bcrypt");
 const { Router } = require("express");
 const { toJWT } = require("../auth/jwt");
-const authMiddleware = require("../auth/middleware");
 const User = require("../models/").user;
 const { SALT_ROUNDS } = require("../config/constants");
 
@@ -19,9 +18,15 @@ router.post("/login", async (req, res, next) => {
 
     const user = await User.findOne({ where: { email } });
 
-    if (!user || !bcrypt.compareSync(password, user.password)) {
+    if (!user) {
       return res.status(400).send({
-        message: "User with that email not found or password incorrect"
+        message: "User with that email not found",
+      });
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      return res.status(400).send({
+        message: "Password incorrect!",
       });
     }
 
@@ -34,7 +39,7 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
-router.post("/signup", async (req, res) => {
+router.post("/register", async (req, res) => {
   const { email, password, name } = req.body;
   if (!email || !password || !name) {
     return res.status(400).send("Please provide an email, password and a name");
@@ -44,7 +49,7 @@ router.post("/signup", async (req, res) => {
     const newUser = await User.create({
       email,
       password: bcrypt.hashSync(password, SALT_ROUNDS),
-      name
+      name,
     });
 
     delete newUser.dataValues["password"]; // don't send back the password hash
@@ -61,15 +66,6 @@ router.post("/signup", async (req, res) => {
 
     return res.status(400).send({ message: "Something went wrong, sorry" });
   }
-});
-
-// The /me endpoint can be used to:
-// - get the users email & name using only their token
-// - checking if a token is (still) valid
-router.get("/me", authMiddleware, async (req, res) => {
-  // don't send back the password hash
-  delete req.user.dataValues["password"];
-  res.status(200).send({ ...req.user.dataValues });
 });
 
 module.exports = router;
